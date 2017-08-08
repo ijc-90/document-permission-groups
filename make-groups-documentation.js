4e94ef8a678e851a21cebd97c473f0b54efffba4
Joako <ijc-90@hotmail.com>
2017-08-08 19:22:13 -0300

    ejemplo funcionando

---
+++ make-groups-documentation.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var groups = require('./groups');
+var resaltadorInicio = "[ACÁ EMPIEZA]";
+var resaltadorFin = "[ACÁ TERMINA]";
+
+
+fs.readFile('./functionalities.md', 'utf8', function (err,data) {
+  if (err) {
+    console.log("error reading functionalities file");
+    return console.log(err);
+  }
+  
+  var functionalities = "";
+
+  groups.forEach(function (group) {
+
+    functionalities = data;
+    console.log(group);
+    
+    group.permissions.forEach(function (permission){
+        console.log(permission);
+        var regexp = new RegExp(permission, "ig");
+        var newValue = resaltadorInicio + permission + resaltadorFin;
+
+        functionalities = functionalities.replace(regexp, newValue);
+    });
+    
+    
+    fs.writeFile("./groupsDocumentation/"+group.name, functionalities, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    }); 
+});
+
+});
+
+
+
